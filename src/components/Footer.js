9f5c870edf60c47079d237d56bed4d5c4303d273
prateek951 <prince.cenation@gmail.com>
2018-04-21 18:03:43 +0530

    added the gitignored files

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Footer = (props) => {
-    return (<h1>This is the footer {props.copyright}</h1> );
-};
-
-export default Footer;
--- src/components/List.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Item} from './Item';
-
-export const List = props => {
-    // List is a collection of items
-    return(<div>{props.items.map(item => <Item item={item}></Item>)}</div>)
-
-}
