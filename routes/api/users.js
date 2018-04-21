a465ab0ec8b6c60094a0f13b91d82d0ddd6bb8af
prateek951 <prince.cenation@gmail.com>
2018-04-21 19:40:59 +0530

    added destructuring

--- routes/api/users.js
+++ routes/api/users.js
@@ -22,25 +22,25 @@ router.get("/test", (req, res) => {
 /*@access Public*/
 
 router.post('/register',(req, res) => {
+    const {name,email,password} = req.body;
     /*First check whether the user you are trying to register is already a registered one
     In that case don't allow*/
-    User.findOne({email: req.body.email})
+    User.findOne({email})
     .then(user => {
       if(user){
         return res.status(400).json({message: 'Email is already in use'});
       }else{
         /*Else case create new user*/
-        const avatar = gravatar.url(req.body.email,{
+        const avatar = gravatar.url(email,{
           s: '200', //size
           r: 'pg', //rating
           d : 'mm' //default
         });
-
         const newUser = new User({
-            name : req.body.name,
-            email : req.body.email,
+            name,
+            email,
             avatar,
-            password : req.body.password
+            password
         });
 
         /*@desc Before storing the user to the database simple hash the password
