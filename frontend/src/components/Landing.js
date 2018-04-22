08aab351fe4d4663404abfb46660cb165663214f
prateek951 <prince.cenation@gmail.com>
2018-04-22 14:59:24 +0530

    a to Link

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,33 +1,35 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-const Landing = () => {
-    return <div className="landing">
-          <div className="dark-overlay landing-inner text-light">
-            <div className="container">
-             <div className="row">
-                <div className="col-md-12 text-center">
-                  <h1 className="display-3 mb-4">SocialU - Socialize and Build Trust and New Relations..</h1>
-                  <p className="lead">
-                      Create a developer profile/portfolio, share posts and
-                  get help   from other developers  {" "}
-                </p>
-                  <hr />
-                  <a href="register.html" className="btn btn-lg btn-info mr-2">
-                    Sign Up  {" "}
-                </a>
-                  <a href="login.html" className="btn btn-lg btn-light">
-                    Login  {" "}
-                </a>
-                 {" "}
-              </div>
-               {" "}
-            </div>
-             {" "}
-          </div>
-           {" "}
-        </div>
-         {" "}
-      </div>;
+class Landing extends Component {
+    render() {
+        return (<div className="landing">
+            <div className="dark-overlay landing-inner text-light">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-12 text-center">
+                    <h1 className="display-3 mb-4">
+                      SocialU - Socialize and Build Trust and New
+                      Relations..
+                    </h1>
+                    <p className="lead">
+                      Create a developer profile/portfolio, share posts
+                      and get help from other developers{" "}
+                    </p>
+                    <hr />
+                    <Link href="register" className="btn btn-lg btn-info mr-2">
+                      Sign Up{" "}
+                    </Link>
+                    <Link to="login" className="btn btn-lg btn-light">
+                      Login{" "}
+                    </Link>{" "}
+                  </div>{" "}
+                </div>{" "}
+              </div>{" "}
+            </div>{" "}
+    </div>
+    );
+    }
 };
 
 export default Landing;
