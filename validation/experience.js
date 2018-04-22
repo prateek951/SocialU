fa1f1fda89237e7fceec20789b0d6a2115d6c905
prateek951 <prince.cenation@gmail.com>
2018-04-22 12:11:21 +0530

    education and experience validation

--- validation/experience.js
+++ validation/experience.js
@@ -2,8 +2,26 @@ const Validator = require('validator');
 const isEmpty = require('./is-empty');
 
 module.exports = function validateExperienceInput(data){
+    let errors = {};
 
+    data.title = isEmpty(data.title) ? '' : data.title;
+    data.company = isEmpty(data.company)? '' : data.company;
+    data.from = isEmpty(data.from) ? '': data.from;
 
-
-    
-}
+    /*Validate whether the title field exists or not*/
+    if(Validator.isEmpty(data.title)){
+        errors.title = 'Title field is required';
+    } 
+    /*Validate whether the company field exists or not*/
+    if(Validator.isEmpty(data.company)){
+        errors.company = 'Company field is required';
+    } 
+    /*Validate whether from field eixsts or not*/
+    if(Validator.isEmpty(data.from)){
+        errors.from = 'From date field is required';
+    }
+    return {
+        errors,
+        isValid : isEmpty(errors)
+    };
+};
