7e9552d55f99c810324de0e9abd420aca97e8f61
prateek951 <prince.cenation@gmail.com>
2018-04-22 14:19:32 +0530

    added static footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+/*@desc
+Root App Component which holds the Header,LandingPage,Footer of the application
+Header and footer is same throughout all the pages.
+*/ 
+
 import React, { Component } from 'react'
+import Header from './components/Header';
+import Landing from './components/Landing';
+import Footer from './components/Footer';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="App">
+        <Navbar/>
+        <Landing/>
+        <Footer/>
       </div>
     )
   }
